e1a0a2f1cf4fd7fb87f10eca4149ab6745a58199
addionff4-byte <addionff4@gmail.com>
2026-06-07 10:42:17 +0530

    Create matches.js

---
+++ app/data/matches.js
@@ -0,0 +1,7 @@
+export const mockPlayers = [
+  { name: "Virat Kohli", role: "Batsman", country: "India", stats: { Test: "8848 runs", ODI: "13842 runs", T20: "4188 runs" } },
+  { name: "Ben Stokes", role: "All-Rounder", country: "England", stats: { Test: "6316 runs", ODI: "3461 runs", T20: "585 runs" } },
+  { name: "Rashid Khan", role: "Bowler", country: "Afghanistan", stats: { Test: "34 wickets", ODI: "185 wickets", T20: "152 wickets" } },
+  { name: "Babur Azam", role: "Batsman", country: "Pakistan", stats: { Test: "3898 runs", ODI: "5729 runs", T20: "4145 runs" } },
+  { name: "Pat Cummins", role: "Bowler", country: "Australia", stats: { Test: "269 wickets", ODI: "141 wickets", T20: "66 wickets" } }
+];
